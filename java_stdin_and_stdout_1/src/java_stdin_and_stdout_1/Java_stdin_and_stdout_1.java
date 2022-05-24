d9d2849b87242c85ce42e954a6e64033cc2813cd
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-24 11:18:15 +0600

    Java Stdin and Stdout I

---
+++ java_stdin_and_stdout_1/src/java_stdin_and_stdout_1/Java_stdin_and_stdout_1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_stdin_and_stdout_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1?isFullScreen=true
+ */
+public class Java_stdin_and_stdout_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        scan.close();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
