e8b7efbd79c3352f41a0f46cba08601c35ad4fb1
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-24 15:51:06 +0600

    Java Loops 2

---
+++ JavaLoops2/src/javaloops2/JavaLoops2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers sc Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template sc the editor.
+ */
+package javaloops2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nadia Mim
+ */
+public class JavaLoops2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        int q = sc.nextInt();
+        
+        for(int i = 0; i < q; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            int series = a;
+            
+            for (int j = 0; j < n; j++){
+                series += (int)(Math.pow (2, j) * b);
+                System.out.printf("%d ", series);  
+            }
+            
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+    
+}
