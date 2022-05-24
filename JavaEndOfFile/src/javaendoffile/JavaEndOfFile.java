dfc43eb3a5b416d74c1a154c7d29458b6c98d22c
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-24 16:51:50 +0600

    Java End of File

---
+++ JavaEndOfFile/src/javaendoffile/JavaEndOfFile.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaendoffile;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-end-of-file/problem?isFullScreen=true
+ */
+public class JavaEndOfFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        
+        for (int i = 1; sc.hasNext() ; i++){
+            
+                System.out.println(i + " " + sc.nextLine());
+            
+        }
+        
+    }
+    
+}
