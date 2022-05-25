79179bbd7c2dc5803b4f7d75719682f5b9380f9b
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-25 15:55:32 +0600

    Java Currency Formatter

--- JavaCurrencyFormatter/src/javacurrencyformatter/JavaCurrencyFormatter.java
+++ JavaCurrencyFormatter/src/javacurrencyformatter/JavaCurrencyFormatter.java
@@ -5,9 +5,14 @@
  */
 package javacurrencyformatter;
 
+import java.text.NumberFormat;
+import java.util.*;
+
 /**
  *
  * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true
  */
 public class JavaCurrencyFormatter {
 
@@ -16,6 +21,23 @@ public class JavaCurrencyFormatter {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        double payment = sc.nextDouble();
+        sc.close();
+        
+        NumberFormat usCurr = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat indiaCurr = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        NumberFormat chinaCurr = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceCurr = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String us = usCurr.format(payment);
+        String india = indiaCurr.format(payment);
+        String china = chinaCurr.format(payment);
+        String france = franceCurr.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
     
 }
