eef80b6b7cc6c80729ec76a803c91a7f104ff0d7
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 10:37:19 +0600

    Java Comparator

---
+++ JavaComparator/src/javacomparator/JavaComparator.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacomparator;
+
+import java.util.*;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-comparator/problem?isFullScreen=true
+ */
+public class JavaComparator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+    
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player>{
+
+    public int compare(Player o1, Player o2) {
+        
+        if(o1.score == o2.score){
+            return o1.name.compareTo(o2.name);
+        }else{
+            return o2.score-o1.score;
+        }
+    }
+    
+}
