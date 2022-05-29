588e0a28d7139e048c9e1e5d981275827303c354
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 13:28:50 +0600

    Java Dequeue

---
+++ JavaDequeue/src/javadequeue/JavaDequeue.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadequeue;
+
+
+import java.util.*;
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-dequeue/problem?isFullScreen=true
+ */
+public class JavaDequeue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner in = new Scanner(System.in);
+        Deque <Integer> deque = new ArrayDeque <> ();
+        HashSet <Integer> set = new HashSet <> ();
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            deque.add(num);
+            set.add(num);
+            
+            if(deque.size() == m){
+                if(set.size() > max){
+                    max = set.size();
+                    int element = deque.remove();
+                    if (!deque.contains(element)){
+                        set.remove(element);
+                    }
+                }
+            }
+
+        }
+        System.out.println(max);
+    }
+    
+}
