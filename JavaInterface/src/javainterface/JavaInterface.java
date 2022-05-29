825c89aa483346b8a2fdd26f91ea8ceda31ec1c7
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 17:41:27 +0600

    Java Interface

---
+++ JavaInterface/src/javainterface/JavaInterface.java
@@ -0,0 +1,48 @@
+
+package javainterface;
+
+import java.util.*;
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
+ */
+public class JavaInterface {
+
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+    }
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic{
+
+    @Override
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            if(n%i == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    
+}
