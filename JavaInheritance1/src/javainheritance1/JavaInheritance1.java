98623f24ba4a57d397a6456ba50dad909cbe03a8
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 16:20:59 +0600

    Java Inheritance I

--- JavaInheritance1/src/javainheritance1/JavaInheritance1.java
+++ JavaInheritance1/src/javainheritance1/JavaInheritance1.java
@@ -18,6 +18,27 @@ public class JavaInheritance1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
     }
-    
+     
 }
+class Animal{
+	void walk()
+	{
+		System.out.println("I am walking");
+	}
+    }
+    class Bird extends Animal
+    {
+        void fly()
+        {
+                System.out.println("I am flying");
+        }
+        void sing()
+        {
+                System.out.println("I am singing");
+        }
+    }
