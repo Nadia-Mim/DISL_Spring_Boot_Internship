a0c840b9dd271149c43a9df9e51234995eb5c7bc
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 17:59:30 +0600

    Java Method Overriding 2 (Super Keyword)

---
+++ JavaMethodOverriding2/src/javamethodoverriding2/JavaMethodOverriding2.java
@@ -0,0 +1,33 @@
+package javamethodoverriding2;
+
+import java.util.*;
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem?isFullScreen=true
+ */
+public class JavaMethodOverriding2 {
+    
+    public static void main(String[] args) {
+        MotorCycle M=new MotorCycle();
+    }
+    
+}
+class BiCycle{
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle{
+    String define_me(){
+        return "a cycle with an engine.";
+    }
+    MotorCycle(){
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+        String temp=super.define_me();
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+	
+}
