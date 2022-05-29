2ea441476fc3e4c1878f41cad25bea85db107182
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 16:25:20 +0600

    Java Inheritance I

--- JavaInheritance1/src/javainheritance1/JavaInheritance1.java
+++ JavaInheritance1/src/javainheritance1/JavaInheritance1.java
@@ -26,19 +26,19 @@ public class JavaInheritance1 {
      
 }
 class Animal{
-	void walk()
-	{
-		System.out.println("I am walking");
-	}
-    }
-    class Bird extends Animal
+    void walk()
     {
-        void fly()
-        {
-                System.out.println("I am flying");
-        }
-        void sing()
-        {
-                System.out.println("I am singing");
-        }
+            System.out.println("I am walking");
     }
+}
+class Bird extends Animal
+{
+    void fly()
+    {
+            System.out.println("I am flying");
+    }
+    void sing()
+    {
+            System.out.println("I am singing");
+    }
+}
---
+++ JavaInheritance2/src/javainheritance2/JavaInheritance2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javainheritance2;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-inheritance-2/problem?isFullScreen=true
+ */
+public class JavaInheritance2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
