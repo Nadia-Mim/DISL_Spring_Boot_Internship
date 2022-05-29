e7d633dd3f2ab1e9e0ad9768fe1a132e0ac23380
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 16:16:29 +0600

    Java Inheritance I

---
+++ JavaInheritance1/src/javainheritance1/JavaInheritance1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javainheritance1;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-inheritance-1/problem?isFullScreen=true
+ */
+public class JavaInheritance1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
