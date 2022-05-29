908209318a50894fac6a2b54ad6603078f78f339
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-29 11:34:20 +0600

    Java Sort

---
+++ JavaSort/src/javasort/JavaSort.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasort;
+
+import java.util.*;
+
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * Hackerrank problem link: https://www.hackerrank.com/challenges/java-sort/problem?isFullScreen=true
+ */
+public class JavaSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(testCases>0){
+                int id = in.nextInt();
+                String fname = in.next();
+                double cgpa = in.nextDouble();
+
+                Student st = new Student(id, fname, cgpa);
+                studentList.add(st);
+
+                testCases--;
+        }
+        Collections.sort(studentList, Comparator.comparing(Student::getCgpa).reversed()
+                .thenComparing(Student::getFname).thenComparing(Student::getId));
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+    }
+    
+}
+
+class Student{
+	private int id;
+	private String fname;
+	private double cgpa;
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+}
