5fbe2946594b81cc1314b0d0f15bd3decd565f71
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-05-31 10:44:27 +0600

    Java SHA-256

---
+++ JavaSHA256/src/javasha256/JavaSHA256.java
@@ -0,0 +1,26 @@
+package javasha256;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+/**
+ *
+ * @author Nadia Mim
+ * 
+ * 
+ */
+public class JavaSHA256 {
+
+    public static void main(String[] args)throws NoSuchAlgorithmException {
+        Scanner sc = new Scanner (System.in);
+        String msg = sc.nextLine();
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        //return byte array of the message digest string input
+        byte [] messageDigest = md.digest(msg.getBytes());
+        
+        for (byte b: messageDigest){
+            System.out.printf ( "%02x", b);
+        }
+    }
+    
+}
