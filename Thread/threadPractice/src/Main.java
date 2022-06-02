7f68e7488b3ac3b3e5adc1fa7f0c89936dac6dc9
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-06-02 17:55:31 +0600

    practiced some thread problems

--- Thread/threadPractice/src/Main.java
+++ Thread/threadPractice/src/Main.java
@@ -1,23 +1,27 @@
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Main extends Thread{
     public static void main(String[] args) {
-        Main st = new Main();
-        st.start();
-        Thread t2 = new Main();
-        t2.start();
-        Thread threadFirst =new Thread("ThreadFirst") ;
-        threadFirst.setPriority(10);
-        System.out.println("Name:" +threadFirst.getName());
-        System.out.println("Priority:" +threadFirst.getPriority());
-        ClassImplementsRunnable td = new ClassImplementsRunnable();
-        Thread runthread = new Thread(td, "runthread");
-        runthread.start();
+//        Main st = new Main();
+//        st.start();
+//        Thread t2 = new Main();
+//        t2.start();
+//        Thread threadFirst =new Thread("ThreadFirst") ;
+//        threadFirst.setPriority(10);
+//        System.out.println("Name:" +threadFirst.getName());
+//        System.out.println("Priority:" +threadFirst.getPriority());
+//        ClassImplementsRunnable td = new ClassImplementsRunnable();
+//        Thread runthread = new Thread(td, "runthread");
+//        runthread.start();
+//
+//        Runnable r = new MyThread();
+//        Thread t = new Thread(r);
+//        System.out.println("Created thread by implementing Runnable interface.");
+//        t.start();
 
-
+        /*calling another class*/
+        multithreadedProgrammimng mp = new multithreadedProgrammimng();
+        mp.InterThreadDemo();
+        srynconizedThreadMethod stm = new srynconizedThreadMethod ();
+        stm.seatReservation();
     }
     public void run(){
         System.out.println("Thread running...");
@@ -27,5 +31,11 @@ class ClassImplementsRunnable implements Runnable{
     @Override
     public void run() {
         System.out.println("I am running");
+    }
+
+}
+class MyThread implements Runnable{
+    public void run(){
+        System.out.println("Thread Running");
     }
 }
---
+++ Thread/threadPractice/src/srynconizedThreadMethod.java
@@ -0,0 +1,52 @@
+public class srynconizedThreadMethod {
+    /*seat reservation*/
+    void seatReservation(){
+        Reservation reserve = new Reservation();
+        Person thread1 = new Person(reserve, 5);
+        thread1.start();
+        Person thread2 = new Person(reserve, 4);
+        thread2.start();
+        Person thread3 = new Person(reserve, 3);
+        thread3.start();
+    }
+}
+
+class Reservation{
+    static int availableSeats = 10;
+    synchronized void reserveSeat(int requestedSeats){
+        System.out.println(Thread.currentThread().getName() + " entered.");
+        System.out.println("Availableseats : " + availableSeats + " Requestedsetas : " + requestedSeats);
+        if (availableSeats >= requestedSeats)
+        {
+            System.out.println("Seat Available. Reserve now :-)");
+            try
+            {
+                Thread.sleep(100);     // LINE E
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("Thread interrupted");
+            }
+            System.out.println(requestedSeats + " seats reserved.");
+            availableSeats = availableSeats - requestedSeats;
+        }
+        else
+        {
+            System.out.println("Requested seats not available :-(");
+        }
+        System.out.println(Thread.currentThread().getName() + " leaving.");
+        System.out.println("----------------------------------------------");
+    }
+}
+class Person extends Thread {
+    Reservation reserve;
+    int requestedSeats;
+
+    public Person(Reservation reserve, int requestedSeats){
+        this.reserve = reserve;
+        this.requestedSeats = requestedSeats;
+    }
+    public void run(){
+        reserve.reserveSeat(requestedSeats);
+    }
+}
